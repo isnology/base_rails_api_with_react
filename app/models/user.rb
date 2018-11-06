8be5c15ef6d97d0ebc1f879742afed1e0a845ddb
isnology <glenn@isnology.com>
2018-11-06 21:09:05 +1100

    add email and admin to jwt payload

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,6 +6,10 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable,
          :jwt_authenticatable, jwt_revocation_strategy: JwtBlacklist
 
+  def jwt_payload
+    { email: email, admin: admin }
+  end
+  
   def on_jwt_dispatch(token, payload)
     JwtBlacklist.where("exp < ?", Date.today).destroy_all
   end
